7db7dc16e6a7ad82ecbc36053f7527cac7203259
DrewDavidson1999 <dmd00020@mix.wvu.edu>
2024-09-16 19:37:32 -0400

    Pushing

---
+++ SampleProject/Pages/WeatherDataHomePage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SampleProject.Pages
+{
+    public class WeatherDataHomePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

128594c8031ddfa993fb90cfd2b88100bcc5b47a
jmb0035 <142920022+jmb0035@users.noreply.github.com>
2024-09-17 15:33:29 -0400

    added the blank historical page

---
+++ SampleProject/Pages/Historical Page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SampleProject.Pages
+{
+    public class Historical_PageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

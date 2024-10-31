e5ab1aa1eceb5763155acb99bde1fc2f00616269
nathan-stryker <156019346+nathan-stryker@users.noreply.github.com>
2024-10-31 18:31:04 -0400

    Added my entities

---
+++ WeatherDataAPI/WeatherDataAppAPI/Entities/ChangePassword.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WeatherDataAppAPI.Entities
+{
+    public class ChangePassword
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string PasswordHash { get; set; }
+
+    }
+}
---
+++ WeatherDataAPI/WeatherDataAppAPI/Entities/UpdatePreferredLocation.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeatherDataAppAPI.Entities
+{
+    public class UpdatePreferredLocation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [MaxLength(50)]
+        public string Location { get; set; }
+    }
+}

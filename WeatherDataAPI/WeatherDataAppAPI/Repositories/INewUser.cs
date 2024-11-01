2115e9b7f0dd784c1380bd9833a9fb00fc362df5
Samanthawilson2 <sjw00015@mix.wvu.edu>
2024-10-31 20:33:33 -0400

    Cleaned up

--- WeatherDataAPI/WeatherDataAppAPI/Repositories/INewUser.cs
+++ WeatherDataAPI/WeatherDataAppAPI/Repositories/INewUser.cs
@@ -5,6 +5,7 @@ using WeatherDataAppAPI.Repositories;
 namespace WeatherDataAppAPI.Repositories
 {
 	public interface INewUser
+
 	{
 
         public async Task<bool> InsertNewUserGetDetails(string userName, string email, string passwordHash);

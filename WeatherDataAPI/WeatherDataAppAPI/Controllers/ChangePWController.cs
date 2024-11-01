85992f6430bc6c0cb870c42f5bbdee16612b7a91
nathan-stryker <156019346+nathan-stryker@users.noreply.github.com>
2024-10-31 22:02:46 -0400

    finished first change password api

---
+++ WeatherDataAPI/WeatherDataAppAPI/Controllers/ChangePWController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherDataAppAPI.Entities;
+using WeatherDataAppAPI.Repositories;
+
+namespace WeatherDataAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChangePWController : Controller
+    {
+        private readonly IChangePW changePW;
+        public ChangePWController(IChangePW changePW)
+        {
+            this.changePW = changePW;
+        }
+        [HttpGet("{userid, passwordhash}")]
+        public async Task<List<ChangePW>> ChangePassword(int userid, string passwordhash)
+        {
+            var password = await changePW.ChangePassword(userid, passwordhash);
+            if (password == null)
+            {
+                // return NotFound();
+            }
+            return password;
+
+        }
+    }
+}

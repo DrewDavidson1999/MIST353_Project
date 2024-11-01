71fde61f838264a9fe51210d49d133bf79f33a09
Samanthawilson2 <sjw00015@mix.wvu.edu>
2024-10-31 21:49:59 -0400

    Cleaned up and updated API readme.

--- WeatherDataAPI/WeatherDataAppAPI/Repositories/INewUser.cs
+++ WeatherDataAPI/WeatherDataAppAPI/Repositories/INewUser.cs
@@ -5,6 +5,7 @@ using WeatherDataAppAPI.Repositories;
 
 namespace WeatherDataAppAPI.Repositories
 {
+
     public interface INewUser
     {
         Task<List<NewUser>> InsertNewUserGetDetails(string userName, string email, string passwordHash);

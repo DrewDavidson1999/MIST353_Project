2d55a2806ed57bfad6b8628832ac1c82b33c6f33
jmb0035 <142920022+jmb0035@users.noreply.github.com>
2024-11-18 18:33:42 -0500

    added delete user and delete location functions to webpage

---
+++ SampleProject/Pages/LDelete/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SampleProject.Data;
+
+namespace SampleProject.Pages.LDelete
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly SampleProject.Data.WeatherDataDbContext _context;
+
+        public DetailsModel(SampleProject.Data.WeatherDataDbContext context)
+        {
+            _context = context;
+        }
+
+        public ExtLocation ExtLocation { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var extlocation = await _context.ExtLocations.FirstOrDefaultAsync(m => m.LocationId == id);
+            if (extlocation == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                ExtLocation = extlocation;
+            }
+            return Page();
+        }
+    }
+}
